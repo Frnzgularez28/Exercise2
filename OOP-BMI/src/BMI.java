e9e379bf99021ef7bfb11400b7be076741976d07
Frnzgularez28 <franzegularez28@gmail.com>
2022-12-08 15:59:10 +0800

    added oop-bmi

---
+++ OOP-BMI/src/BMI.java
@@ -0,0 +1,37 @@
+public class BMI  extends Athlete{
+    public BMI(String name, double weight, double height) {
+        super(name, height, weight);
+    }
+
+    public String diagnostic(){
+        double bmiRAW = calculateBMI();
+        String Result = name+" has ";
+        String strBMI = String.format("%.02f", bmiRAW);
+
+        if (bmiRAW <= 16) {
+            Result += "Very severe low weight";
+        } else if (bmiRAW >= 16 && bmiRAW <= 16.99) {
+            Result += "Severe low weight";
+        } else if (bmiRAW >= 17 && bmiRAW <= 18.49) {
+            Result += "Low weight";
+        } else if (bmiRAW >= 18.50 && bmiRAW <= 24.99) {
+            Result += "Normal weight";
+        } else if (bmiRAW >= 25 && bmiRAW <= 29.99) {
+            Result += "Overweight";
+        } else if (bmiRAW >= 30 && bmiRAW <= 34.99) {
+            Result += "Grade I obesity";
+        } else if (bmiRAW >= 35 && bmiRAW <= 39.99) {
+            Result += "Grade II obesity";
+        } else if (bmiRAW >= 40) {
+            Result += "Grade III obesity(morbid obesity)";
+        }
+
+        Result += " BMI: "+strBMI+"kg/m²";
+        return Result;
+    }
+
+    public double calculateBMI(){
+        return weight / (height*height);
+    }
+
+}
